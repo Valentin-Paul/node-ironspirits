568c751339ab57b0f7e996ff25a717f27649e319
Valentin Paul <valentin@vamptech.de>
2022-04-20 12:46:39 +0200

    added handlebar and products

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
 const express = require('express');
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs")
+
 app.use(express.static('public'));
 
 // app.get(path, code);
-
+app.get("/", (req, res, next)=> {
+    res.sendFile(__dirname + '/views/home.html');
+})
 
 app.get("/about", (req, res, next) => {
-    console.log(req.url);
-    console.log("a request on the ABOUT page was received... ")
-
     res.sendFile(__dirname + '/views/about.html');
 });
 
+app.get("/contact", (req, res, next) => {
+    res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get("/limoncello", (req, res, next) => {
+
+    const data = {
+        title: 'Limoncello',
+        price: 20,
+        image: 'productLimoncello.jpeg',
+        stores: ['Online', 'Madrid', 'Berlin', 'Paris']
+    }
+
+    res.render('product',data)
+});
+
+app.get("/whisky", (req, res, next) => {
+
+    const data = {
+        title: 'Single Malt',
+        price: 90,
+        image: 'singleMalt.webp'
+    }
+    
+    res.render('product',data)
+});
+
+app.get("/tequila", (req, res, next) => {
+
+      const data = {
+        title: 'Tequila',
+        price: 35,
+        image: 'tequila.jpeg'
+    }
+    
+    res.render('product',data)
+});
 
 app.listen(3001, () => {
     console.log("server listening to requests...")
